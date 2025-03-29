087a5bf6f0012ce16b57c7d29558b640c2ccbff8
sirliboyev-uz <sirliboyevuz@gmail.com>
2025-03-29 12:18:51 +0500

    requirements.txt

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # ✅ Add this block right after creating `app`
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["https://web-ar-frontend-e3ahvy28y-sirliboyevuzs-projects.vercel.app/"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
